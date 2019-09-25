835b4949dc3c6bf509f53ad053dd52470e773688
Amritpal Dhami <30017389@student.toiohomai.ac.nz>
2019-09-25 12:08:07 +1200

    msg

---
+++ TEST2.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write { "Input employee first name :");
+                string fname = Console.ReadLine();
+                Console.Write("input employee last name:");
+                string lname = Console.ReadLine();
+                Console.Write(" input Employee annual salary:");
+                double salary = double.Parse(Console.ReadLine());
+                Console.Write("input employee wages :");
+                double wage = double.Parse(Console.ReadLine());
+                 Console.WriteLine();
+                Employee al = new Employee(fname, lname, salary, wages:);
+                 Console.WriteLine($"Net Salary: {al.Netsalary()}");
+        }
+    }
+        class SALARY
+        {
+            string fname;
+            string lname;
+            double salary;
+            double weeklysalary;
+            public double Salary { get { return salary; } set { salary = 80000; } }
+            public double Weeklysalary { get { return Weeklysalary; } set { Weeklysalary = value; } }
+     
+            //dISPLAY THE METHOD FOR SALARY//
+            //NUMBER OF WEEKS IN A YEAR IS 365/7 = 52
+            public double dISPLAYsALARY;
+            double 80000/52;
+               return $" PER WEEK SALARY IS ${Math.round(income, 2)}";
+                //create the constructor//
+
+                public SALARY()
+            {
+                Console.WriteLine(" your salary is 80000 per year "):
+                Salary = 8000;
+            }
+            
+
+            public class wages
+            {
+                // variabls//
+                private double hourlyrate;
+                private double numhors;
+                private double weeklywages;
+                public double Hourlyrate { get { return hourlyrate ; } set { hourlyrate = 33.72 } }
+                public double numhoUrs; y { get { return numhors; } set { Weeklysalary = numhors;}
+                public double numhoUrs; y { get { return numhors; } set { Weeklysalary = numhors;}
+    }
+            }
+            
+
+              
+            
+             
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
